1cce8ee1222c552b34ad8cfcb409b2517fb8c4fa
fahmi <fahmilukis04@gmail.com>
2026-01-04 07:18:28 +0700

    feat(network): change to lisk sepolia

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrumSepolia } from 'viem/chains';
+import { liskSepolia } from 'viem/chains';
 
 export const config = getDefaultConfig({
   appName: 'NoCall Protocol',
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains: [arbitrumSepolia],
+  chains: [liskSepolia],
   ssr: true,
 });
